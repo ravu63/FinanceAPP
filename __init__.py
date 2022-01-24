befb8d73aec1ec666a3fd92639b953b18c4f9ed1
joemama-debug <79206925+joemama-debug@users.noreply.github.com>
2022-01-24 16:26:43 +0800

    changed something in recaptcha

--- __init__.py
+++ __init__.py
@@ -27,6 +27,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 app.static_folder = 'static'
 
+app.config['RECAPTCHA_USE_SSL'] = False
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
+app.config['RECAPTCHA_OPTIONS'] = {'theme': 'black'}
+
 RECAPTCHA_PUBLIC_KEY = "6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J"
 RECAPTCHA_PRIVATE_KEY = "6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu"
 
